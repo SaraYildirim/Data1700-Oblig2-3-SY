7451d809ab30df47a674d48456b49ca187c4774d
SaraYildirim <s352530@oslomet.no>
2024-04-16 11:47:56 +0200

    Initial commit

---
+++ src/main/java/com/example/data1700oblig23sy/Data1700Oblig23SyApplication.java
@@ -0,0 +1,13 @@
+package com.example.data1700oblig23sy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Data1700Oblig23SyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Data1700Oblig23SyApplication.class, args);
+	}
+
+}
